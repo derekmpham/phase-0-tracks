1d277bd8a2e6c896f37ccb7e2180167f3d81d4fa
Derek Pham <derekmpham@gmail.com>
2016-10-04 23:17:14 -0700

    Add interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+p "What is your name?"
+name=gets.chomp
+
+p "How old are you?"
+age=gets.chomp.to_i
+
+p "What year were you born?"
+dob=gets.chomp.to_i
+
+p "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+garlic=gets.chomp
+
+p "Would you like to enroll in the company’s health insurance? (Y/N)"
+health=gets.chomp
