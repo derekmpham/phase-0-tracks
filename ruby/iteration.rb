53759fc615280acd1c56c9af21cc1a70611a96d3
Derek Pham <derekmpham@gmail.com>
2016-10-10 14:05:03 -0700

    Find methods for release 2 tasks

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -26,3 +26,55 @@ dev_module.each do |mod, subject|
 end
 
 puts dev_module
+
+
+# WORKING WITH DELETE_IF
+
+numbers = [1, 2, 3, 4, 5, 8, 3, 6, 9]
+
+# array demonstration
+numbers.delete_if { |numbers| numbers < 4 }
+p numbers
+
+# hash demonstration
+dev_module.delete_if { |key, value| key < 3 }
+p dev_module
+
+
+# WORKING WITH KEEP_IF
+
+# array demonstration
+numbers = [1, 2, 3, 4, 5, 8, 3, 6, 9]
+numbers.keep_if { |numbers| numbers < 4 }
+p numbers
+
+# hash demonstration
+dev_module = {1 => "Git", 2 => "HTML", 3 => "CSS", 4 => "Ruby"}
+dev_module.keep_if { |key, value| key < 3 }
+p dev_module
+
+
+# WORKING WITH SELECT
+
+# array demonstration
+numbers = [1, 2, 3, 4, 5, 8, 3, 6, 9]
+numbers.select! { |numbers| numbers < 4 }
+p numbers
+
+# hash demonstration
+dev_module = {1 => "Git", 2 => "HTML", 3 => "CSS", 4 => "Ruby"}
+dev_module.select! { |key, value| key < 3 }
+p dev_module 
+
+
+# WORKING WITH DROP_WHILE
+
+# array demonstration
+numbers = [1, 2, 3, 4, 5, 8, 3, 6, 9]
+numbers = numbers.drop_while { |numbers| numbers < 4 }
+p numbers
+
+# hash demonstration
+dev_module = {1 => "Git", 3 => "CSS", 4 => "Ruby", 2 => "HTML"}
+dev_module.delete_if { |key, value| key < 3 ? true : break }
+p dev_module
