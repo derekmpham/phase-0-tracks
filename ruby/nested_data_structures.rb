0fc2acf2e83c7b5091c49e317037731023ace21e
Derek Pham <derekmpham@gmail.com>
2016-10-11 22:01:07 -0700

    Create nested structure for laker roster

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,42 @@
+# LA LAKER ROSTER
+
+roster = {
+	guards: {
+		young: [
+			"Jordan Clarkon",
+			"Julian Jacobs",
+			"D'Angelo Russell"
+		],
+		veterans: [
+			"Jose Calderon",
+			"Marcelo Huertas",
+			"Louis Williams",
+			"Nick Young"
+		]
+	}
+	forwards: {
+		young: [
+			"Zach Auguste",
+			"Anthony Brown",
+			"Brandon Ingram",
+			"Larry Nance Jr.",
+			"Julius Randle",
+			"Travis Wear"
+		],
+		veterans: [
+			"Luol Deng",
+			"Yi Jianlian",
+			"Thomas Robinson",
+			"Metta World Peace"
+		]
+	}
+	centers: {
+		young: [
+			"Tarik Black",
+			"Ivica Zubac"
+		],
+		veterans: [
+			"Timofey Mozgov"
+		]
+	}
+}
