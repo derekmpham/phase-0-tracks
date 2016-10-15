eaf7e63fdeb880536e4f92faab4e21fc648cf6b2
Derek Pham <derekmpham@gmail.com>
2016-10-15 10:29:02 -0700

    Add implement simple search method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,36 +1,45 @@
 # Implement simple search
-# arr = [14, 73, 5, 85, 21]
+arr = [1,2,3,4,9,5,7,55]
 
-# def search_array()
-	 # string.each_index {|x| p x }
-# end	
-# p search_array(arr)
+puts "Please select number in array"
+no = gets.chomp.to_i
+
+def search_array (arr, no)
+answer = arr.each_index.select{|x| no == arr[x]}
+  answer
+end
+
+p search_array(arr, no)
 
 
 # Calculate fibonacci numbers
 def fibonnaci(num_terms)
-	# create an array
-	array = []
-	index = 0
-	until num_terms > 0
-		puts "Please put number greater than 0"
-		num_terms = gets.chomp.to_i
-	end
-	if num_terms == 1
-		array.push(0)
-	else
-		array.push(0)
-		array.push(1)
-		until index == (num_terms - 2)
-			array.push(array[index] + array[index.next])
-			p index
-			index += 1
-		end
-	end
-	array
+  array = []
+  index = 0
+  # if user puts 0 or negative integer
+  while num_terms <= 0
+    puts "Please put number greater than 0"
+    num_terms = gets.chomp.to_i
+  end
+  # if user puts num_terms==1
+  if num_terms == 1
+    array.push(0)
+  # main algorithm for fibonacci sequence
+  else
+    # start off with 0 and 1 in sequence
+    array.push(0)
+    array.push(1)
+    while index != (num_terms - 2)
+      # next number would be sum of two previous numbers in sequence
+      array.push(array[index] + array[index.next])
+      index += 1
+    end
+  end
+  array
 end
+ 
 
-puts "How many terms would you like to have in your Fibonnaci sequence?"
+puts "How many terms would you like in your Fibonnaci sequence?"
 num_terms = gets.chomp.to_i
 
 p fibonnaci(num_terms)
