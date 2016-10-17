2b071ccec10d5a52aecdeb2f21e33a7c444b416f
Derek Pham <derekmpham@gmail.com>
2016-10-17 16:41:07 -0700

    Design puppy class via comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#BREED-----------------
+#Beagle
+
+#CHARACTERISTICS-------
+#Weight: 10.0 - 11.3 kg
+#Height: 33 - 41 cm
+#Coat: Short haired, hard coat of medium length
+#Color: Tricolor or white in combination with black & tan/brown or brown/tan
+
+#BEHAVIOR
+#Hunt via scent
+#Merry
+#Friendly
+#Curious
