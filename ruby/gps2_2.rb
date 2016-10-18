b1837951ec10da562fbdf53d9ca2e87119c13c25
Derek Pham <derekmpham@gmail.com>
2016-10-17 19:37:38 -0700

    Wrote out all methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -26,3 +26,52 @@
 # steps: iterate through our list (hash)
 # output: print out each item and respective quantity on list
 
+
+# Method 1: create a list
+def create_list(items)
+	grocery_hash = {}
+	split_items = items.split(' ')
+	split_items.each do |item|
+		grocery_hash[item] = 1
+	end
+	p grocery_hash
+end
+
+g_list = create_list("carrots apples cereal pizza")
+
+
+# Method 2: add item
+def add_item(list, item_name, quantity = 1)
+	list[item_name] = quantity
+	p list
+end
+
+add_item(g_list, "bananas", 2)
+
+
+# Method 3: remove item
+def remove_item(list, item_name)
+	list.delete(item_name)
+	p list
+end
+
+remove_item(g_list, "carrots")
+
+
+# Method 4: update quantity of item
+def quant_update(list, item_name, quantity)
+	list[item_name] = quantity
+	p list
+end
+
+quant_update(g_list, "apples", 5)
+
+
+# Method 5: print everything
+def pretty_hash(list)
+	list.each do |item, quantity|
+		p "#{item}: #{quantity}"
+	end
+end
+
+pretty_hash(g_list)
