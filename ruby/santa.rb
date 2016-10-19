cf95211af0040f7fb1da2297f03069f6ca13593e
Derek Pham <derekmpham@gmail.com>
2016-10-18 21:40:09 -0700

    Add a list of santas to Santa class through an array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,7 @@
-#DEFINE SANTA CLASS
+#SANTA CLASS
 class Santa
 	def speak
-		puts "#Ho, ho, ho! Haaaappy holidays! I am an #{@ethnicity} #{@gender} Santa! I am #{@age} years old."
+		puts "#Ho, ho, ho! Haaaappy holidays! I am a #{@gender} #{@ethnicity} Santa! I am #{@age} years old."
 	end
 
 	def eat_milk_and_cookies(cookie)
@@ -17,9 +17,27 @@ class Santa
 	end
 end
 
+#create new array, and add two arrays inside
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+#insert santas (including its genders and ethnicities arrays) array into Santa class
+genders.length.times do |i|
+	santas << Santa.new(genders[i], ethnicities[i])
+end
+
+
+
 
 #DRIVER CODE
 
+#initial test: add new instance titled "nicole"
 p nicole = Santa.new("female", "Italian")
 nicole.speak
 nicole.eat_milk_and_cookies("chocolate chip")
+
+#Have each item (a.k.a. individual santas) in santas array "speak"
+santas.each do |i|
+	i.speak
+end
