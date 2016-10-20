4095b3dd65726845cbdd1062117a7594e5d505f1
Derek Pham <derekmpham@gmail.com>
2016-10-20 15:27:00 -0700

    Make mixin version of Shout module, and write AngryDriver and Friend classes and include Shout module to them

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,18 +1,39 @@
 #SHOUT MODULE
 module Shout
-  
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+	
+  	#yelling angrily and happily methods
+  	# def self.yell_angrily(words)
+  	#   words + "!!!" + " :("
+  	# end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :D"
+  	# def self.yelling_happily(words)
+  	# 	words + "!!!" + " :D"
+  	# end
+
+  def shouting(calling)
+  	puts "Hey! #{calling}"
   end
 
 end
 
+#ADD TWO CLASSES THAT USE SHOUT MODULE
+class AngryDriver
+	include Shout
+end
+
+class Friend
+	include Shout
+end
+
+
 
 
 #DRIVER CODE
-p Shout.yell_angrily("Hey")
-p Shout.yelling_happily("Hey")
+	# p Shout.yell_angrily("Hey")
+	# p Shout.yelling_happily("Hey")
+#driver code for AngryDriver class
+truck_driver = AngryDriver.new
+truck_driver.shouting("Watch where you're going!")
+#driver code for Friend class
+jim = Friend.new
+jim.shouting("How's it going?!")
