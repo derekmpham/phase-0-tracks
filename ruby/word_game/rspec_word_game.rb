1153ee171b28d8ab1cd6c33eafd9bbeab03acd09
Derek Pham <derekmpham@gmail.com>
2016-10-22 17:48:48 -0700

    Add specs for check letter method

--- ruby/word_game/rspec_word_game.rb
+++ ruby/word_game/rspec_word_game.rb
@@ -1,13 +1,49 @@
 require_relative "word_game"
 
 describe WordGame do
-	let(:game) {WordGame.new("hi")}
+	let(:game) {WordGame.new("hello")}
 
 	it "stores the list items given on initialization" do
-		expect(game.secret_word).to eq "hi"
-		expect(game.guesses_left).to eq 4
-		# expect(game.underscored_word).to eq "__"
+		expect(game.secret_word).to eq "hello"
+		expect(game.guesses_left).to eq 10
+		expect(game.underscored_word).to eq "_____"
+		expect(game.guessed_letters).to eq []
+		expect(game.win).to eq false
+		expect(game.lose).to eq false
 	end
 
+	it "checks if input letter guess is part of the secret word" do
+		game.check_letter("l")
+		expect(game.underscored_word).to eq "__ll_"
+		expect(game.guesses_left).to eq 10
+		game.check_letter("l")
+		expect(game.guesses_left).to eq 10
+		game.check_letter("w")
+		expect(game.guesses_left).to eq 9
+		expect(game.win).to eq false
+		expect(game.lose).to eq false
+	end
+
+	it "checks if Player 2 can win" do
+		game.check_letter("h")
+		game.check_letter("e")
+		game.check_letter("l")
+		game.check_letter("o")
+		expect(game.win).to eq true
+	end
+
+	it "checks if Player 2 can lose" do
+		game.check_letter("q")
+		game.check_letter("w")
+		game.check_letter("r")
+		game.check_letter("t")
+		game.check_letter("y")
+		game.check_letter("u")
+		game.check_letter("i")
+		game.check_letter("p")
+		game.check_letter("a")
+		game.check_letter("s")
+		expect(game.lose).to eq true
+	end
 
 end
