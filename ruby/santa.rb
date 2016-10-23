1020a90ae43ea20a98e3f802d84e72a25e328157
Derek Pham <derekmpham@gmail.com>
2016-10-23 13:16:24 -0700

    Add first three methods and first four attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,3 +1,36 @@
+#SANTA CLASS
 class Santa
+	
+	def initialize(gender, ethnicity)
+		p "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+	end
+
+	def reindeer_ranking
+		p @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+	end
+
+	def age
+		p @age = 0
+	end
+
+	def speak
+		p "Ho, ho, ho! Haaaappy holidays!"
+	end
+
+	def eat_milk_and_cookies (cookie_type)
+		p "That was a good #{cookie_type} cookie!"
+	end
 
 end
+
+
+
+
+#DRIVER CODE
+nick = Santa.new("male", "Vietnamese")
+nick.speak
+nick.eat_milk_and_cookies("chocolate chip")
+nick.reindeer_ranking
+nick.age
