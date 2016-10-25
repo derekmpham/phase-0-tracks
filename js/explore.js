c363f7c4ca2d8ef85f868dd6531c5d2512be0eee
Derek Pham <derekmpham@gmail.com>
2016-10-24 19:01:38 -0700

    Add pseudocode for reverse function

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+//PSEUDOCODE-->reverse string function
+	//delete first letter of index-->equate this deleted letter to a variable
+	//insert deleted variable back into string
+	//loop this process until we've done this as many times as the length of the string
+
+
