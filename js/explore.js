f3fd4f1baba08e50b4872cebdf8af5687278dd83
Derek Pham <derekmpham@gmail.com>
2016-10-24 21:52:33 -0700

    Add reverse function and corresponding driver codes

--- js/explore.js
+++ js/explore.js
@@ -1,6 +1,28 @@
-//PSEUDOCODE-->reverse string function
-	//delete first letter of index-->equate this deleted letter to a variable
-	//insert deleted variable back into string
-	//loop this process until we've done this as many times as the length of the string
+//PSEUDOCODE: reverse string function
+	//create empty string
+	//create loop iteration
+		//create an index variable that starts at the last character of the word and counts down one at a time for every loop iteration until it reaches 0 (index 0 = first character of word)
+		//insert index of word (which starts at the last character of the word) into the empty string for every loop iteration
 
 
+//REVERSE FUNCTION
+
+function reverse(word) {
+	var reversedWord = "";
+	for (var i = word.length - 1; i >= 0; i--) {
+		reversedWord += word[i];
+	}
+	return reversedWord;
+}
+
+var isTired = true;
+
+
+
+
+//DRIVER CODE
+console.log(reverse("hello")); //-->reverse function: expect "olleh"
+
+if (isTired) {
+	console.log(reverse("hello")); //add silly conditional statement: expect "olleh"
+}
