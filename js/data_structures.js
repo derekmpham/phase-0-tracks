d735ee76c009e1a766196cb99b8e5fb2589e99f2
Derek Pham <derekmpham@gmail.com>
2016-10-26 19:15:53 -0700

    Create object and merge two arrays within object

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,9 +4,14 @@ var names = ["Ed", "Misty", "Farrow", "Blackie"];
 colors.push("orange");
 names.push("Comet");
 
+var coloredHorse = {}
 
+for (var i = 0; i < names.length; i++) {
+	coloredHorse[names[i]] = colors[i];
+}
 
 
 //DRIVER CODE
 console.log(colors);
 console.log(names);
+console.log(coloredHorse);
