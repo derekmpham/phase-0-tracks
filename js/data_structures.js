c67729e32c8aa073c80d7e26534fbea627e400a2
Derek Pham <derekmpham@gmail.com>
2016-10-26 18:47:04 -0700

    Create color and name arrays and add new array items to each

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colors = ["blue", "red", "green", "yellow"];
+var names = ["Ed", "Misty", "Farrow", "Blackie"];
+
+colors.push("orange");
+names.push("Comet");
+
+
+
+
+//DRIVER CODE
+console.log(colors);
+console.log(names);
