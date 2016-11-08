eb66463f555ab7ca3fef2c4cabd2496ac61d84e4
Derek Pham <derekmpham@gmail.com>
2016-11-08 14:27:40 -0800

    Add mouse-hover event listener

--- web_dev/activepage.js
+++ web_dev/activepage.js
@@ -3,3 +3,15 @@ p1 = p[0]
 p1.style.color= "deepskyblue";
 p1.style.border= "2px solid gold";
 
+function changepixelcolor(event) { 
+event.target.style.backgroundColor= "yellow";
+}
+
+function changepixelcolorback(event) { 
+event.target.style.backgroundColor= "whitesmoke";
+}
+
+var body = document.getElementsByTagName("body");
+body1 = body[0]
+body1.addEventListener("mouseover", changepixelcolor);
+body1.addEventListener("mouseout", changepixelcolorback);
